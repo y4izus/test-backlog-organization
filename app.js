949b6e03c2004d35acebf7f2fd1e9c3a6a22f38f
Yaiza García <yaiza.garcia@ibm.com@yaizas-mbp-7.home>
2019-10-25 14:46:33 +0200

    Sets port to run on heroku

--- app.js
+++ app.js
@@ -19,4 +19,4 @@ app.get('/', (req, res) => {
     res.send('HOME')
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
